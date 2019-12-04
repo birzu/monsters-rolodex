d4288551cbd52ff0296a08cd5704ce3e4ca89c07
subashmp <subash.mahapatra44@gmail.com>
2019-12-04 22:14:32 +0530

    added styles to app, heading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+import './App.scss'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchField: '',
+      robots: [],
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App__heading">Monster Rolodex</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
