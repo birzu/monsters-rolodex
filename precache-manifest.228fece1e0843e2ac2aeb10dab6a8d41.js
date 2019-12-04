8112b328e4bc2819c1fbc6248d38a07e2680fa5d
subashmp <subash.mahapatra44@gmail.com>
2019-12-05 03:09:07 +0530

    Updates

---
+++ precache-manifest.228fece1e0843e2ac2aeb10dab6a8d41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b69310fabb1d36c4c48836f678c66b7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "06d15b675e7988670c49",
+    "url": "/monsters-rolodex/static/css/main.9cef867e.chunk.css"
+  },
+  {
+    "revision": "77b6eb19321035b5a7e0",
+    "url": "/monsters-rolodex/static/js/2.61a19b5e.chunk.js"
+  },
+  {
+    "revision": "06d15b675e7988670c49",
+    "url": "/monsters-rolodex/static/js/main.e9f5b009.chunk.js"
+  },
+  {
+    "revision": "9192ce65241d38f9d8e9",
+    "url": "/monsters-rolodex/static/js/runtime-main.5e2c316b.js"
+  }
+]);
